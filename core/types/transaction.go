a6aa81ecb71e042b9baccdb3574d3a35ae1f4bf2
Beni Ben Zikry <bbenzikry@gmail.com>
2024-03-18 02:17:46 +0200

    support blast system tx

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -206,6 +206,8 @@ func (tx *Transaction) decodeTyped(b []byte) (TxData, error) {
 		inner = new(DynamicFeeTx)
 	case BlobTxType:
 		inner = new(BlobTx)
+	case 126:
+		inner = new(DynamicFeeTx)
 	default:
 		return nil, ErrTxTypeNotSupported
 	}
